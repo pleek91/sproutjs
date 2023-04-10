787dccbffa6626c35754f1bd057133ba6d8c4065
Craig Harshbarger <pleek91@gmail.com>
2023-04-09 22:29:37 -0500

    Add a comment about how to support locals

--- src/mocks/timezone.ts
+++ src/mocks/timezone.ts
@@ -1,5 +1,7 @@
 import { choice } from '@/utilities'
 
+// todo: enhance with Local.timeZones when globally available
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Locale/timeZones
 export function timezone(): string {
   const values = Intl.supportedValuesOf('timeZone')
 
