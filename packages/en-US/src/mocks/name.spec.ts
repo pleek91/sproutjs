ca92756b192f2a248950ce16103165a4c0d78772
Craig Harshbarger <pleek91@gmail.com>
2023-04-12 23:12:55 -0500

    Fix name format logic. Support m (middle) and i (initial) tokens

--- packages/en-US/src/mocks/name.spec.ts
+++ packages/en-US/src/mocks/name.spec.ts
@@ -11,7 +11,7 @@ describe('name', () => {
   })
 
   it('respects format argument', () => {
-    const value = name({ format: 'f m. l' })
+    const value = name({ format: 'f i. l' })
 
     expect(value).toMatch(/^[a-zA-Z]+ [A-Z]\. [a-zA-Z]+$/)
   })
--- packages/en-US/src/mocks/name.ts
+++ packages/en-US/src/mocks/name.ts
@@ -10,7 +10,9 @@ export type NameArgs = {
 export function name({ gender = 'any', format = 'f l' }: NameArgs = {}): string {
   const first = firstName({ gender })
   const last = lastName()
-  const middle = letter()
+  const middle = firstName({ gender })
+  const initial = letter()
+  const template = format.replace('f', '1').replace('i', '2').replace('m', '2').replace('l', '3')
 
-  return format.replace('f', first).replace('m', middle).replace('l', last)
+  return template.replace('1', first).replace('2', initial).replace('3', middle).replace('4', last)
 }
